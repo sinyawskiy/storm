b55e19f9da76eda2bbe2ad1a7175cbb7c9b10cfc
als <als@sats.spb.ru>
2016-04-27 10:58:18 +0300

    add supervisor bash script

--- application.py
+++ application.py
@@ -310,5 +310,5 @@ def search(query):
 #             result += u'%s' % card
 #     return result
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port='8000')
+# if __name__ == '__main__':
+app.run(host='127.0.0.1', port='8000')
